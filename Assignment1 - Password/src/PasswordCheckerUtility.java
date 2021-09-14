b3af2ab16699693fee5727c88d6ff98ce7e7e75b
alexmatosdev <60074634+alexmatosdev@users.noreply.github.com>
2021-09-14 02:02:34 -0400

    Add files via upload

--- Assignment1 - Password/src/PasswordCheckerUtility.java
+++ Assignment1 - Password/src/PasswordCheckerUtility.java
@@ -1,20 +1,42 @@
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+/**
+ * 
+ * This class checks for valid and invlaid passwords
+ * 
+ * @author Alex Matos
+ *
+ */
 public class PasswordCheckerUtility {
 
 
+	/**
+	 * 
+	 * @param password
+	 * @return true if password's length is between 6 and 9 characters
+	 */
 	public static boolean hasBetweenSixAndNineChars(String password) {
 		return password.length() >=6 && password.length() <= 9;
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return true if password contains a digit
+	 * @throws NoDigitException
+	 */
 	public static boolean hasDigit(String password) throws NoDigitException {
 		for (int i = 0; i < password.length(); i++)
 			if (!(password.contains("1") || password.contains("2") || password.contains("3") || password.contains("4") || password.contains("5") || password.contains("6")|| password.contains("7") || password.contains("8")|| password.contains("9") || password.contains("0")))
 				throw new NoDigitException();
 		return true;
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return true if password contains an uppercase character
+	 * @throws NoLowerAlphaException
+	 */
 	public static boolean hasLowerAlpha(java.lang.String password) throws NoLowerAlphaException {
 		for (int i = 0; i < password.length(); i++)
 			if (password.charAt(i)>=97 && password.charAt(i)<=122)
@@ -22,21 +44,36 @@ public class PasswordCheckerUtility {
 		throw new NoLowerAlphaException();
 		
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return if password has the same char more than twice in a row
+	 * @throws InvalidSequenceException
+	 */
 	public static boolean hasSameCharInSequence(java.lang.String password) throws InvalidSequenceException {
 		for (int i = 0; i < password.length()-2; i++)
 			if ((password.charAt(i+1) == password.charAt(i)) && password.charAt(i) == password.charAt(i+2))
 				throw new InvalidSequenceException();
 		return true;
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return true if password has a special character
+	 * @throws NoSpecialCharacterException
+	 */
 	public static boolean hasSpecialChar(java.lang.String password) throws NoSpecialCharacterException {
 		for (int i = 0; i < password.length(); i++)
 			if ((password.charAt(i)>=33 && password.charAt(i)<=47) || password.charAt(i) == 64)
 				return true;
 		throw new NoSpecialCharacterException();
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return true if password has an uppercase letter
+	 * @throws NoUpperAlphaException
+	 */
 	public static boolean hasUpperAlpha(java.lang.String password)
             throws NoUpperAlphaException {
 		for (int i=0; i<password.length(); i++)
@@ -44,14 +81,29 @@ public class PasswordCheckerUtility {
 				return true;
 		throw new NoUpperAlphaException();
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return true if password's length is greater than or equal to 6
+	 * @throws LengthException
+	 */
 	public static boolean isValidLength(java.lang.String password)
             throws LengthException {
 		if (password.length() >= 6)
 			return true;
 		throw new LengthException();
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return true if password passes all requirements
+	 * @throws LengthException
+	 * @throws NoUpperAlphaException
+	 * @throws NoLowerAlphaException
+	 * @throws NoDigitException
+	 * @throws NoSpecialCharacterException
+	 * @throws InvalidSequenceException
+	 */
 	public static boolean isValidPassword(java.lang.String password)
             throws LengthException,
                    NoUpperAlphaException,
@@ -67,7 +119,18 @@ public class PasswordCheckerUtility {
 		if (hasSameCharInSequence(password) == false) {throw new InvalidSequenceException();}	
 		return false;
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @return false if password is not weak
+	 * @throws WeakPasswordException
+	 * @throws LengthException
+	 * @throws NoUpperAlphaException
+	 * @throws NoLowerAlphaException
+	 * @throws NoDigitException
+	 * @throws NoSpecialCharacterException
+	 * @throws InvalidSequenceException
+	 */
 	public static boolean isWeakPassword(java.lang.String password)
             throws WeakPasswordException, LengthException, NoUpperAlphaException, NoLowerAlphaException, NoDigitException, NoSpecialCharacterException, InvalidSequenceException {
 		if (isValidPassword(password) && password.length() <= 9)
@@ -75,16 +138,30 @@ public class PasswordCheckerUtility {
 		
 		return false;
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @param passwordConfirm
+	 * @throws UnmatchedException
+	 */
 	public static void comparePasswords(String password, String passwordConfirm) throws UnmatchedException {
 		if (!password.equals(passwordConfirm))
 			throw new UnmatchedException();
 	}
-	
+	/**
+	 * 
+	 * @param password
+	 * @param passwordConfirm
+	 * @return true if passwords are the same
+	 */
 	public static boolean comparePasswordsWithReturn(String password, String passwordConfirm) {
 		return password.equals(passwordConfirm);
 	}
-	
+	/**
+	 * 
+	 * @param passwords
+	 * @return all the invalid passwords from an array
+	 */
 	public static ArrayList<String> getInvalidPasswords(ArrayList<String> passwords) {
 		ArrayList<String> invalidPasswords = new ArrayList<>();
 		for (String password : passwords) {
